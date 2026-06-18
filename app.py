7112f08b2ea7233277290b0684509c901e6fe461
Boopathi <bb4969344@gmail.com>
2026-06-18 16:01:52 +0530

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -100,7 +100,10 @@ def translate():
 
 
 # ─── Run Server ──────────────────────────────────────────────
+import os
+
 if __name__ == "__main__":
     print("🌐 Language Translation Tool is running!")
-    print("👉 Open: http://127.0.0.1:5000")
-    app.run(debug=True, host="127.0.0.1", port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
